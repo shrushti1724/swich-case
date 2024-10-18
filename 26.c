33698e4c1d09c8deaf71e7a5197bc471acfebfd3
shrushti1724 <shrustipatel0817@gmail.com>
2024-10-18 18:20:38 +0530

    Add files via upload

---
+++ 26.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int num;
+	printf("enter the 1-12 numer:");
+	scanf("%d",&num);
+	
+	switch(num)
+	{
+		case 1 :printf("january");
+		break;
+		case 2 :printf("february");
+		break;
+		case 3 :printf("march");
+		break;
+		case 4 :printf("april");
+		break;
+		case 5 :printf("may");
+		break;
+		case 6 :printf("june");
+		break;
+		case 7:printf("july");
+		break;
+		case 8 :printf("august");
+		break;
+		case 9:printf("september");
+		break;
+		case 10 :printf("october");
+		break;
+		case 11 :printf("november");
+		break;
+		case 12 :printf("december"); 
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
